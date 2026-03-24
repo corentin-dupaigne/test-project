4c47aeb93766a4f499b2f16050e6267638ba3e1e
corentin.dupaigne <corentin.dupaigne@epita.fr>
2026-03-24 21:38:42 +0100

    chore: Add comments

--- part_3/conftest.py
+++ part_3/conftest.py
@@ -1,7 +1,9 @@
 import pytest
 from utils.driver_factory import create_driver
 
-
+"""
+    Opens a browser before each test, gives it to the test, then closes it when the test is done.
+"""
 @pytest.fixture(scope="function")
 def driver():
     d = create_driver()
